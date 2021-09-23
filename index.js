ab957119abd26314db758191f2fca4ea99abca11
Toby Cook <tobycook@enucs.org.uk>
2021-09-24 00:09:25 +0100

    Add fetch for people in gym

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-const { Builder } = require('selenium-webdriver');
+const { Builder, By, Key, withTagName, until } = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
+const assert = require('assert/strict');
 
-(async function myFunction() {
+(async function puregym() {
     const options = new firefox.Options();
     options.headless();
-
     const driver = await new Builder()
         .forBrowser('firefox')
         .setFirefoxOptions(options)
         .build();
+    try {
+        await driver.get('https://www.puregym.com/members/');
 
-    await driver.get('https://tobycook.dev');
+        await driver.findElement(By.name('username')).sendKeys('<USER_EMAIL>');
+        await driver.findElement(By.name('password')).sendKeys('<USER_PIN>', Key.ENTER);
+
+        const isDisplayed = await driver.wait(until.elementLocated(By.id('people_in_gym')), 10000).isDisplayed();
+        assert(isDisplayed === true);
+
+        const numbersElem = await driver.findElement(By.id('people_in_gym'));
+        const peopleInGym = await driver.findElement(withTagName('span').near(numbersElem));
+
+        console.log(await peopleInGym.getText())
+    } finally {
+        await driver.quit();
+    }
 })();
   
